24c26fbf645aec3df22af5b6ada39da8c64e6457
Rebecca Ivans <ivansr@my.erau.edu>
2015-10-22 00:02:45 -0400

    NoMatch draft

--- app/src/main/java/edu/erau/eagleeye/NoMatch.java
+++ app/src/main/java/edu/erau/eagleeye/NoMatch.java
@@ -1,7 +1,12 @@
 package edu.erau.eagleeye;
 
-import android.support.v7.app.AppCompatActivity;
+// Rebecca Ivans
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class NoMatch extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class NoMatch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_no_match);
+
+        Button newPicButton = (Button) findViewById(R.id.newPicButton);
+        newPicButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        //This is where I open the OtherActivity by clicking the button
+                        Intent myIntent = new Intent(NoMatch.this, TakePicture.class);
+                        NoMatch.this.startActivity(myIntent);
+                    }
+                }
+        );
     }
 }
+
