73a31fdd5008d4539ad645fdc22f8fb9a9d5c5c3
lagasai <lagasai@my.erau.edu>
2015-10-25 22:27:48 -0400

    TakePicture complete Sprint1

--- app/src/main/java/edu/erau/eagleeye/TakePicture.java
+++ app/src/main/java/edu/erau/eagleeye/TakePicture.java
@@ -6,7 +6,6 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -24,7 +23,7 @@ public class TakePicture extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
 
-        photoFileDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "eagleEye");
+        photoFileDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "eagle_eye");
         photoFileDir.mkdirs();
     }
 
@@ -33,7 +32,7 @@ public class TakePicture extends AppCompatActivity {
     public void onTakePicturePressed(View v) {
 
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        photoFile = new File(photoFileDir, "eagleEyePhoto.jpg");
+        photoFile = new File(photoFileDir, "eagle_eye_photo.jpg");
         photoUri = Uri.fromFile(photoFile);
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
         startActivityForResult(cameraIntent, PICTURE_TAKEN);
@@ -46,7 +45,13 @@ public class TakePicture extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICTURE_TAKEN && photoFile.exists()) {
-
+            if (BlackBox.isMatch(photoUri, photoUri)) {
+                Intent changeToFound = new Intent(this, FoundBuilding.class);
+                startActivity(changeToFound);
+            } else {
+                Intent changeToNotFound = new Intent(TakePicture.this, NoMatch.class);
+                TakePicture.this.startActivity(changeToNotFound);
+            }
         }
 
     }
