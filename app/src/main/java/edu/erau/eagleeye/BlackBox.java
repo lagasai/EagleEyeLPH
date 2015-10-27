96dc74a3828a1054bb2f0151c1de677d66a9d8b0
waelchlr <waelchlr@my.erau.edu>
2015-10-27 18:16:59 -0400

    Changed the construction of the isMatch method so that it returns matches every other try.

--- app/src/main/java/edu/erau/eagleeye/BlackBox.java
+++ app/src/main/java/edu/erau/eagleeye/BlackBox.java
@@ -1,18 +1,23 @@
 package edu.erau.eagleeye;
+
+import android.net.Uri;
+import java.util.Random;
+
 /**
  * Date: Oct 27-2015
  * BlackBox class is a simulation of the 3rd party, pattern matching software that we hope to
  * purchase or find for later integration.  This is meant to be only a temporary component of
  * the EagleEye program.
- * @author Waelchli
+ * @author Robert Waelchli
  * @version 1.0
  * @see BlackBox
  */
-
-import android.net.Uri;
-import java.util.Random;
-
 public class BlackBox {
+
+    /**
+     * this global variable enables isMatch to return matches only every second time its called
+     */
+    private static int methodCounter=1;
 
     /**
      * this is the public constructor for BlackBox class
@@ -32,10 +37,21 @@ public class BlackBox {
         boolean matchTrueFalse;
 
         //instantiate new Random object
-        Random decider = new Random();
+        //Random decider = new Random();
 
         //assign boolean variable true or false randomly
-        matchTrueFalse=decider.nextBoolean();
+        //matchTrueFalse=decider.nextBoolean();
+
+        //assign boolean variable true or false depending if methodCounter variable is odd or even
+        if((methodCounter%2)==0){
+            matchTrueFalse=true;  //return match on even attempts
+        }
+            else{
+            matchTrueFalse=false; //return false on odd attempts
+        }
+
+        //update counter
+        methodCounter=methodCounter+1;
 
         //return true or false value
         return matchTrueFalse;
