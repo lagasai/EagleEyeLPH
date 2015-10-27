ccd8c68c3c5cdc9c5f98505ace51b7f96d9db0a3
Jcel <c16james.combs@usafa.edu>
2015-10-27 18:56:21 -0400

    Database Commit

--- app/src/main/java/edu/erau/eagleeye/INode.java
+++ app/src/main/java/edu/erau/eagleeye/INode.java
@@ -1,7 +1,60 @@
 package edu.erau.eagleeye;
 
+import android.net.Uri;
+
 /**
- * Created by Ian on 10/21/2015.
+ * Created by Mike R on 10/6/2015.
+ * Edited by James Combs on 10/17/2015.
  */
 public class INode {
+
+    //Class attributes
+    //Image image: Image held in node
+    private Uri image;
+    //String name: Name of building associated with image
+    private String name;
+    //INode next: Next INode in the linked list
+    private INode next;
+
+    /**
+     * Basic Constructor
+     * @param n Name of the building associated with image
+     * @param i Image to be held in INode
+     */
+    public void INode(String n, Uri i){
+        name = n;
+        image = i;
+    }
+
+    /**
+     * Provides a link to next INode in linked list
+     * @param a INode to be linked to
+     */
+    public void setNext(INode a){
+        next = a;
+    }
+
+    /**
+     * Returns the image stored in INode
+     * @return Returns the image
+     */
+    public Uri getImageUri(){
+        return this.image;
+    }
+
+    /**
+     * Returns the building name stored in INode
+     * @return Returns the building name
+     */
+    public String getName(){
+        return this.name;
+    }
+
+    /**
+     * Returns the next INode
+     * @return Returns the next INode
+     */
+    public INode getNext(){
+        return this.next;
+    }
 }
--- app/src/main/java/edu/erau/eagleeye/ReferenceImageDatabase.java
+++ app/src/main/java/edu/erau/eagleeye/ReferenceImageDatabase.java
@@ -1,7 +1,52 @@
 package edu.erau.eagleeye;
 
+import android.net.Uri;
+
 /**
- * Created by Ian on 10/21/2015.
+ * Created by Mike R on 10/6/2015.
+ * Edited by James Combs on 10/7/2015
  */
 public class ReferenceImageDatabase {
+
+    //Class attributes
+    //INode root: First INode in the queue
+    public INode root;
+    //INode last: Last INode in the queue
+    public INode last;
+
+    //Simple constructor for the Linked List
+    //Creates a Linked List with a single INode
+    //@@Arg INode r: First INode of the Linked List
+    public void RefrenceImageDatabase(INode r){
+        //Sets root to r
+        root = r;
+        //Sets last to r
+        //r is currently only node in Linked List
+        last = r;
+    }
+
+    //Adds an INode to the end of the Linked List
+    //@@Arg INode n: INode to be added
+    public void addINode(INode n){
+        //Links the current end of the list to n
+        last.setNext(n);
+        //Sets the end of the list to n
+        last = n;
+    }
+
+    public Uri passImage(INode p){
+        return p.getImageUri();
+    }
+
+    public void passDatabase(INode r, Uri[] table){
+        //NEED TO COMPLETE passImage(INode p) TO
+        //COMPLETE METHOD
+        if(r.getNext() != null){
+            passDatabase(r.getNext(), table);
+        }
+        passImage(r);
+    }
+    public void removeNextImage(){
+
+    }
 }
