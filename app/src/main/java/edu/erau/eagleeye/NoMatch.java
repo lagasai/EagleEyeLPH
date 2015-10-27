82204a35a40c0c09d47cb8c962065a62bf4bc951
Rebecca Ivans <ivansr@my.erau.edu>
2015-10-27 17:05:34 -0400

    Added more comments

--- app/src/main/java/edu/erau/eagleeye/NoMatch.java
+++ app/src/main/java/edu/erau/eagleeye/NoMatch.java
@@ -1,7 +1,5 @@
 package edu.erau.eagleeye;
 
-// Rebecca Ivans
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,9 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.os.Process;
 
-
+/**
+ * @author Rebecca Ivans
+ * @version 1.0
+ *
+ * This class, NoMatch, is an activity that displays when no match has been found for the user
+ * image.  It displays a short message along with the Eagle Eye logo.  In following versions, the
+ * logo will be replaced with the user image.
+ */
 
 public class NoMatch extends AppCompatActivity {
+
+    /**
+     * This method overrides the onCreate method from AppCompatActivity.  It sets the content view
+     * to the correct layout, activity_no_match.xml, and provides listeners for the "Take Another
+     * Picture" and "Quit" buttons in that layout
+     *
+     * @param savedInstanceState A Bundle containing the activity's previously frozen state
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +33,11 @@ public class NoMatch extends AppCompatActivity {
         //Set GUI to correct xml file
         setContentView(R.layout.activity_no_match);
 
-        //Take picture again button listener
+        /*
+        The Button newPicButton is instantiated and identified as the newPicButton in the layout,
+         and a listener is set to capture and handle "click" events for this button by opening the
+         "TakePicture" activity
+         */
         Button newPicButton = (Button) findViewById(R.id.newPicButton);
         newPicButton.setOnClickListener(
                 //Create object of OnClickListener and assign to the Button
@@ -33,17 +50,21 @@ public class NoMatch extends AppCompatActivity {
                 }
         );
 
-        //Quit button listener
+        /*
+        The Button quitButton is instantiated and identified as the quitButton in the layout,
+         and a listener is set to capture and handle "click" events for this button by opening the
+         Home Screen of the device and killing the Eagle Eye app
+         */
         Button quitButton = (Button) findViewById(R.id.quitButton);
         quitButton.setOnClickListener(
                 new Button.OnClickListener() {
                     //Create object of OnClickListener and assign to the Button
                     public void onClick(View v) {
                         //This is where I "close the app" by returning to the home screen
-                        Intent quiterIntent = new Intent(Intent.ACTION_MAIN);
-                        quiterIntent.addCategory(Intent.CATEGORY_HOME);
-                        startActivity(quiterIntent);
-                        //This line actually kills the app (I think)
+                        Intent quitterIntent = new Intent(Intent.ACTION_MAIN);
+                        quitterIntent.addCategory(Intent.CATEGORY_HOME);
+                        startActivity(quitterIntent);
+                        //This line actually kills the app
                         Process.killProcess(Process.myPid());
                     }
                 }
