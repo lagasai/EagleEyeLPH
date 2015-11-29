00ba753e2ded7c1334c7a38cd71f54e6ba648d77
lagasai <lagasai@my.erau.edu>
2015-11-29 16:44:28 -0500

    Sample test case, needs to be tested

--- app/src/androidTest/java/edu/erau/eagleeye/ApplicationTest.java
+++ app/src/androidTest/java/edu/erau/eagleeye/ApplicationTest.java
@@ -7,7 +7,23 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    Application testApp;
+
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        createApplication();
+        testApp = getApplication();
+    }
+
+    public void testTest() {
+        Comparator cTest = new Comparator();
+        cTest.attemptMatch(testApp, 0);
+    }
+
 }
